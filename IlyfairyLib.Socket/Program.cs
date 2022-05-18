1b8f74c590e9a89af5d7b9bb1de36be20f7c1fbe
小仙女 <me@ilyfairy.com>
2022-05-18 12:01:09 +0800

    添加项目文件。

---
+++ IlyfairyLib.Socket/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace IlyfairyLib.Sockets
+{
+    public class SocketServer : SocketBase
+    {
+        public SocketServer(string ip, int port)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new DnsEndPoint("1.1.1.1", 80));
+        }
+    }
+
+    public class SocketClient : SocketBase
+    {
+        
+    }
+
+    public class SocketBase
+    {
+        public Socket socket;
+    }
+
+}
